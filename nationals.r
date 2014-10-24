77bbbdcce01f4a4b9211c2ab1e197092ff06d527
Andrew Yale <a.yale9@gmail.com>
2014-10-23 21:11:49 -0400

    organize

--- nationals.r
+++ nationals.r
@@ -1,8 +1,8 @@
-nattys <- read.csv("nattys.csv")
-
-ultilive <- read.csv("ultilive.csv")
+nattys <- read.csv("data/nattys.csv")
+ultilive <- read.csv("data/ultilive.csv")
 
 
 nattys.date.trim <- nattys[as.Date(nattys$Posted.at) >= "2014-10-16" & as.Date(nattys$Posted.at) <= "2014-10-19",]     
-
 ultilive.date.trim <- ultilive[as.Date(ultilive$Posted.at) >= "2014-10-16" & as.Date(ultilive$Posted.at) <= "2014-10-19",]     
+
+
