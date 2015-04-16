26286952ceff7ae579a948115ddb989fd2f778c3
Andrew Yale <a.yale9@gmail.com>
2015-04-15 21:39:05 -0400

    add barplot

--- nationals.py
+++ nationals.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import datetime as dt
-import matplotlib as mp
+import numpy as np
+import matplotlib.pyplot as plt
 from pandas.tseries.offsets import *
 import seaborn as sns
+
+# sns settings
+sns.set(style="darkgrid", context="talk")
 
 
 
@@ -12,6 +16,7 @@ ultilive = pd.read_csv('data/ultilive.csv', parse_dates=['Posted at'])
 # change index
 ultilive.set_index('Posted at', inplace = True)
 
+# timezone change
 ultilive.index = ultilive.index - DateOffset(hours=5)
 
 # cut out screen name and id
@@ -27,10 +32,22 @@ ultilive['minute'] = ultilive.index.minute
 # sort
 ultilive = ultilive.sort_index(ascending=True)
 
+# slice data
 start = ultilive.index.searchsorted(dt.datetime(2014, 10, 16))
 end = ultilive.index.searchsorted(dt.datetime(2014, 10, 20))
-
 ultilive = ultilive.ix[start:end]
 
+# get hours stats
 hours = ultilive.groupby('hour')
-sns.barplot(hours, hours.size())
+
+# plot valuse
+x_hours = np.array(sorted(ultilive.hour.unique()))
+y_values = np.array(hours.size())
+# plot of hours
+sns.barplot(x_hours, y_values, palette="Set2")
+
+
+
+
+
+
