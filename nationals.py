f30e35ff7392dc27bab93405670a0c8385872c6d
Andrew Yale <a.yale9@gmail.com>
2015-04-16 21:40:32 -0400

    add twenty minute chart

--- nationals.py
+++ nationals.py
@@ -59,7 +59,7 @@ def barplotHours(ultilive, nattys):
 
     for i in xrange(24):
         dfHours.Hour[i] = i
-        dfHours.Source[i] = 'Ultilive'
+        dfHours.Source[i] = 'Ultiworld Live'
         
         dfHours.Hour[i+24] = i
         dfHours.Source[i+24] = '#NationalsTX'
@@ -67,9 +67,14 @@ def barplotHours(ultilive, nattys):
     
         if i in ulti_hours:
             dfHours.Tweets[i] = ulti_values[ulti_hours.index(i)]
+        else:
+            dfHours.Tweets[i] = 0
 
         if i in natty_hours:
             dfHours.Tweets[i+24] = natty_values[natty_hours.index(i)]
+        else:
+            dfHours.Tweets[i+24] = 0
+            
 
 
     # sns settings
@@ -77,10 +82,51 @@ def barplotHours(ultilive, nattys):
     plt.figure(figsize=(20,15))
     
     s = sns.barplot('Hour', 'Tweets', 'Source', data=dfHours, palette='Paired')
+    s.set_title("Tweets by Hour")
 
     # save output
     s.figure.savefig("ByHour.png")
 
+    print dfHours
+
+
+def twentyMins(ultilive, nattys):
+    # create array for every 20 minutes from 10-16 00:00 to 10-20 00:00
+    ulti_twentyMins = np.zeros(288)
+
+    for u in ultilive.iterrows():
+        day = (u[1]['day'] - 16)*72
+        hour = (u[1]['hour'])*3
+        min = (u[1]['minute'])%3
+
+        ulti_twentyMins[day+hour+min]+=1
+
+
+
+    # create array for every 20 minutes from 10-16 00:00 to 10-20 00:00
+    natty_twentyMins = np.zeros(288)
+
+    for u in nattys.iterrows():
+        day = (u[1]['day'] - 16)*72
+        hour = (u[1]['hour'])*3
+        min = (u[1]['minute'])%3
+        
+        natty_twentyMins[day+hour+min]+=1
+
+    sns.set(style='darkgrid', context='poster')
+    plt.figure(figsize=(20,15))
+
+
+    s = sns.tsplot(ulti_twentyMins, color="hls")
+    s = sns.tsplot(natty_twentyMins, color="Set2")
+    s.set_ylabel("Tweets")
+    s.set_title("Tweets per Twenty Minutes")
+    s.set_xticks([72, 144, 216, 287])
+    s.set_xticklabels(['17th', '18th', '19th', '20th'])
+    s.set_xlabel("Day")
+
+
+    s.figure.savefig("ByTwenty.png")
 
 
 
@@ -90,10 +136,9 @@ def barplotHours(ultilive, nattys):
 ultilive = parseData('ultilive')
 nattys = parseData('nattys')
 
-#plot tweets per hour
-barplotHours(ultilive, nattys)
+# plot tweets per hour
+#barplotHours(ultilive, nattys)
 
-
-
-
+# scatterplot of tweets throughout the time
+twentyMins(ultilive, nattys)
 
